fd4c7f748cc6a5a6df6dc0a22b5e82dd0a30972a
Pedro <pbruno1283@gmail.com>
2021-10-13 13:24:42 -0300

    Snackbar adiocionada

--- app/src/main/java/com/example/maxima/fragments/DadosFragment.kt
+++ app/src/main/java/com/example/maxima/fragments/DadosFragment.kt
@@ -1,16 +1,21 @@
 package com.example.maxima.fragments
 
-import android.content.Context
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.lifecycle.ViewModelProvider
 import com.example.maxima.R
 import com.example.maxima.data.Cliente
 import com.example.maxima.databinding.FragmentDadosBinding
 import com.example.maxima.viewModel.FragmentDadosViewModel
+import com.google.android.material.snackbar.Snackbar
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class DadosFragment : Fragment() {
 
@@ -24,7 +29,7 @@ class DadosFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentDadosBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -63,9 +68,34 @@ class DadosFragment : Fragment() {
             textViewNomeContato.text = cliente.contatos[0].nome
             textViewTelefone.text = cliente.contatos[0].telefone
             textViewEmail.text = cliente.contatos[0].e_mail
+            textViewTipo.text = cliente.contatos[0].tipo
+            textViewTime.text = cliente.contatos[0].time
+
+            buttonStatus.setOnClickListener {
+                setSnackBar(cliente.status)
+            }
+
         }
-
-
     }
+
+    private fun setSnackBar(status: String) {
+        val snackbar =
+            Snackbar.make(requireView(), getTimeNow() + " - " + status, Snackbar.LENGTH_LONG)
+        snackbar.anchorView = requireView().findViewById(R.id.bottom_navigation)
+        snackbar.setAction("Fechar", View.OnClickListener {
+            fun onClick(v: View) {
+                snackbar.dismiss()
+            }
+        })
+        snackbar.setActionTextColor(resources.getColor(R.color.text_action))
+        snackbar.show()
+    }
+
+    private fun getTimeNow(): String {
+        val date = Calendar.getInstance().time
+        val dateTimeFormat = SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault())
+        return dateTimeFormat.format(date)
+    }
+
 
 }
